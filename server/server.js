7fe015f852b43a71e57a12795f8edd540c405e6b
Mayank Gorshi <gorshimayank@gmail.com>
2026-09-12 20:07:14 +0530

    Make restaurant trial opt-in

--- server/server.js
+++ server/server.js
@@ -628,9 +628,9 @@ async function registerRestaurant(payload = {}) {
      subscription_ends_at
    )
    VALUES (
-     $1,$2,$3,$4,$5,$6,$7,$8,'trialing',
-     CURRENT_TIMESTAMP,
-     CURRENT_TIMESTAMP + INTERVAL '30 days'
+     $1,$2,$3,$4,$5,$6,$7,$8,'trial_available',
+     NULL,
+     NULL
    )
    RETURNING *`,
       [
